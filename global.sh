c2113cbb2f68b87a966041bf5869232a01d4a884
Per Lundh <plundh@perlundh.com>
2014-09-19 21:25:06 +0200

    Add global file to repo..

--- global.sh
+++ global.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 # Foreground (text) colors
-export black="\[$(tput setaf 0)\]"
-export red="\[$(tput setaf 1)\]"
-export green="\[$(tput setaf 2)\]"
-export yellow="\[$(tput setaf 3)\]"
-export blue="\[$(tput setaf 4)\]"
-export magenta="\[$(tput setaf 5)\]"
-export cyan="\[$(tput setaf 6)\]"
-export white="\[$(tput setaf 7)\]"
+black="\[$(tput setaf 0)\]"
+red="\[$(tput setaf 1)\]"
+green="\[$(tput setaf 2)\]"
+yellow="\[$(tput setaf 3)\]"
+blue="\[$(tput setaf 4)\]"
+magenta="\[$(tput setaf 5)\]"
+cyan="\[$(tput setaf 6)\]"
+white="\[$(tput setaf 7)\]"
 
 # Background colors
-export bgblack="\[$(tput setab 0)\]"
-export bgred="\[$(tput setab 1)\]"
-export bggreen="\[$(tput setab 2)\]"
-export bgyellow="\[$(tput setab 3)\]"
-export bgblue="\[$(tput setab 4)\]"
-export bgmagenta="\[$(tput setab 5)\]"
-export bgcyan="\[$(tput setab 6)\]"
-export bgwhite="\[$(tput setab 7)\]"
+bgblack="\[$(tput setab 0)\]"
+bgred="\[$(tput setab 1)\]"
+bggreen="\[$(tput setab 2)\]"
+bgyellow="\[$(tput setab 3)\]"
+bgblue="\[$(tput setab 4)\]"
+bgmagenta="\[$(tput setab 5)\]"
+bgcyan="\[$(tput setab 6)\]"
+bgwhite="\[$(tput setab 7)\]"
 
 # Special stuff
-export bold="\[$(tput bold)\]"
-export hbright="\[$(tput dim)\]"
-export beg_underline="\[$(tput smul)\]"
-export end_underline="\[$(tput rmul)\]"
+bold="\[$(tput bold)\]"
+hbright="\[$(tput dim)\]"
+beg_underline="\[$(tput smul)\]"
+end_underline="\[$(tput rmul)\]"
 
-export reset="\[$(tput sgr0)\]"
+reset="\[$(tput sgr0)\]"
 
